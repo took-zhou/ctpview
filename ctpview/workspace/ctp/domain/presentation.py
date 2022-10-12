937fb179f2f69e8b38e89c460becba5edbe7c984
zhoufan <zhoufan@cdsslh.com>
2022-10-12 02:41:39 +0000

    [问题/需求描述]:将simnow替换成integration [解决方法]:将simnow替换成integration [影响范围]:无 [需求编号]:无
    Change-Id: I0bdcca351ed0d1678b3c1fba29f582cf8d9845d3

--- ctpview/workspace/ctp/domain/presentation.py
+++ ctpview/workspace/ctp/domain/presentation.py
@@ -1,19 +1,21 @@
-import streamlit as st
 import os
 
-from ctpview.workspace.ctp.domain.components.parameter_page import parameter_page
+import streamlit as st
 from ctpview.workspace.ctp.domain.components.control_page import control_page
 from ctpview.workspace.ctp.domain.components.debug_page import debug_page
+from ctpview.workspace.ctp.domain.components.parameter_page import parameter_page
+from ctpview.workspace.ctp.domain.components.setting_page import setting_page
 from ctpview.workspace.ctp.domain.components.status_page import status_page
 from ctpview.workspace.ctp.domain.components.update_page import update_page
-from ctpview.workspace.ctp.domain.components.setting_page import setting_page
+
 
 class ctpview():
+
     def __init__(self):
         st.set_page_config(page_title='tsaodai ctp operation control', layout='centered', page_icon="..")
 
     def update(self):
-        if os.environ.get('run_mode') == 'debug' or 'simnow' in os.environ.get('base_url'):
+        if os.environ.get('base_url') in ['debug', 'integration_test']:
             module_option = st.sidebar.radio('modue', ('configure', 'control', 'status', 'debug', 'update', 'setting'))
         else:
             module_option = st.sidebar.radio('modue', ('configure', 'control', 'status', 'update', 'setting'))
@@ -30,6 +32,7 @@ class ctpview():
             update_page.update()
         elif module_option == 'setting':
             setting_page.update()
+
 
 app = ctpview()
 app.update()
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ctpview",
-    version="1.4.8",
+    version="1.4.9",
     author="zhoufan",
     author_email="zhoufan@cdsslh.com",
     keywords='presentation layer',
