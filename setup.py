c8d07b30c9cddf8dd810c2260b9f0572c592a559
zhoufan <zhoufan@cdsslh.com>
2023-02-12 13:38:18 +0000

    [问题/需求描述]:升级策略到1.5.5 [解决方法]:升级策略到1.5.5 [影响范围]:无 [需求编号]:无
    Change-Id: I255c5ac82d11f02a0c2384317a11ae99a0542b81

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ctpview",
-    version="1.5.4",
+    version="1.5.5",
     author="zhoufan",
     author_email="zhoufan@cdsslh.com",
     keywords='presentation layer',
