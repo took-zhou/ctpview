bd79c4be6eaf88f0a2a06f5c0e3c29ab5f34ff67
zhoufan <zhoufan@cdsslh.com>
2023-03-10 08:18:51 +0000

    [问题/需求描述]:适配取消策略进程标识 [解决方法]:适配取消策略进程标识 [影响范围]:无 [需求编号]:无
    Change-Id: I9adb24cef66c20584754e4c0a9bf5e85706edb68

--- ctpview/workspace/ctp/domain/components/backtest_page.py
+++ ctpview/workspace/ctp/domain/components/backtest_page.py
@@ -11,10 +11,7 @@ from ctpview.workspace.ctp.infra.sender.proxy_sender import proxysender
 class backtest:
 
     def __init__(self):
-        self.para_control_json = {}
         self.backtest_control_json = {}
-        self.prids = []
-        self.prid = ''
 
     def update(self):
         self.backtest_control_json = {}
@@ -27,28 +24,16 @@ class backtest:
         except:
             pass
 
-        try:
-            market_control_path = jsonconfig.get_config('market', 'ControlParaFilePath')
-            with open('%s/controlPara/control.json' % (market_control_path), 'r', encoding='utf8') as fp:
-                self.para_control_json = json.load(fp)
-                fp.close()
-                self.prids = list(set(self.para_control_json['prid']))
-        except:
-            self.prids = []
-            pass
-
-        self.prid = st.selectbox('prids', self.prids, key='prids')
-
         self.operation()
 
     def operation(self):
         contain = st.container()
         col1, col2, col3 = contain.columns(3)
-        if self.prid in self.backtest_control_json:
-            begin = col1.text_input('begin time', self.backtest_control_json[self.prid]['begin'])
-            end = col2.text_input('end time', self.backtest_control_json[self.prid]['end'])
-            speed = col3.number_input('speed', self.backtest_control_json[self.prid]['speed'])
-            now = self.backtest_control_json[self.prid]['now']
+        if self.backtest_control_json != {}:
+            begin = col1.text_input('begin time', self.backtest_control_json['begin'])
+            end = col2.text_input('end time', self.backtest_control_json['end'])
+            speed = col3.number_input('speed', self.backtest_control_json['speed'])
+            now = self.backtest_control_json['now']
         else:
             begin = col1.text_input('begin time', '2015-01-01 09:00:00')
             end = col2.text_input('end time', '2020-12-31 15:00:00')
@@ -64,17 +49,6 @@ class backtest:
         level1_iteration1.text(f'Iteration {process}, date: %s' % (str(now_date).split(' ')[0]))
         level1_bar.progress(process)
 
-        st.write('single')
-        contain = st.container()
-        col1, col2, col3 = contain.columns(3)
-        if col1.button('start', key='start1'):
-            self.hand_single_start_button(begin, end, speed)
-        if col2.button('stop', key='stop1'):
-            self.hand_single_stop_button()
-        if col3.button('finish', key='finish1'):
-            self.hand_single_finish_button()
-
-        st.write('total')
         contain = st.container()
         col1, col2, col3 = contain.columns(3)
         if col1.button('start', key='start2'):
@@ -84,11 +58,10 @@ class backtest:
         if col3.button('finish', key='finish2'):
             self.hand_total_finish_button()
 
-    def hand_single_start_button(self, begin, end, speed):
+    def hand_total_start_button(self, begin, end, speed):
         topic = "ctpview_market.BackTestControl"
         msg = cmp.message()
         mbc = msg.backtest_control
-        mbc.process_random_id = self.prid
         mbc.begin_time = begin
         mbc.end_time = end
         mbc.speed = speed
@@ -99,45 +72,6 @@ class backtest:
         msg = cmp.message()
         mtsi = msg.tick_start_stop_indication
         mtsi.type = cmp.TickStartStopIndication.MessageType.start
-        mtsi.process_random_id = self.prid
-        msg_bytes = msg.SerializeToString()
-        proxysender.send_msg(topic, msg_bytes)
-
-    def hand_single_stop_button(self):
-        topic = "ctpview_market.TickStartStopIndication"
-        msg = cmp.message()
-        mtsi = msg.tick_start_stop_indication
-        mtsi.type = cmp.TickStartStopIndication.MessageType.stop
-        mtsi.process_random_id = self.prid
-        msg_bytes = msg.SerializeToString()
-        proxysender.send_msg(topic, msg_bytes)
-
-    def hand_single_finish_button(self):
-        topic = "ctpview_market.TickStartStopIndication"
-        msg = cmp.message()
-        mtsi = msg.tick_start_stop_indication
-        mtsi.type = cmp.TickStartStopIndication.MessageType.finish
-        mtsi.process_random_id = self.prid
-        msg_bytes = msg.SerializeToString()
-        proxysender.send_msg(topic, msg_bytes)
-
-    def hand_total_start_button(self, begin, end, speed):
-        for prid in self.prids:
-            topic = "ctpview_market.BackTestControl"
-            msg = cmp.message()
-            mbc = msg.backtest_control
-            mbc.process_random_id = prid
-            mbc.begin_time = begin
-            mbc.end_time = end
-            mbc.speed = speed
-            msg_bytes = msg.SerializeToString()
-            proxysender.send_msg(topic, msg_bytes)
-
-        topic = "ctpview_market.TickStartStopIndication"
-        msg = cmp.message()
-        mtsi = msg.tick_start_stop_indication
-        mtsi.type = cmp.TickStartStopIndication.MessageType.start
-        mtsi.process_random_id = ""
         msg_bytes = msg.SerializeToString()
         proxysender.send_msg(topic, msg_bytes)
 
@@ -146,7 +80,6 @@ class backtest:
         msg = cmp.message()
         mtsi = msg.tick_start_stop_indication
         mtsi.type = cmp.TickStartStopIndication.MessageType.stop
-        mtsi.process_random_id = ""
         msg_bytes = msg.SerializeToString()
         proxysender.send_msg(topic, msg_bytes)
 
@@ -155,7 +88,6 @@ class backtest:
         msg = cmp.message()
         mtsi = msg.tick_start_stop_indication
         mtsi.type = cmp.TickStartStopIndication.MessageType.finish
-        mtsi.process_random_id = ""
         msg_bytes = msg.SerializeToString()
         proxysender.send_msg(topic, msg_bytes)
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ctpview",
-    version="1.5.6",
+    version="1.5.8",
     author="zhoufan",
     author_email="zhoufan@cdsslh.com",
     keywords='presentation layer',
