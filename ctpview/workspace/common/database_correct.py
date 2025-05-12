257d59d3a4d9c9345b3b77f46f9b47ce21ba9d43
zhoufan <zhoufan@tsaodai.com>
2025-05-12 19:19:06 +0800

    [问题/需求描述]:完善 [解决方法]:完善 [影响范围]:无 [需求编号]:无
    Change-Id: I14b33b678e62285c712033267acfef33ae8f42e1

--- ctpview/workspace/common/database_correct.py
+++ ctpview/workspace/common/database_correct.py
@@ -11,25 +11,7 @@ class DatabaseCorrect():
         return self.fix1()
 
     def fix1(self):
-        trader_db_path = '/home/tsaodai/data/trader/control.db'
-
-        try:
-            conn = sqlite3.connect(trader_db_path)
-            command = 'alter table order_lookup rename to order_lookup_temp;'
-            conn.execute(command)
-            command = 'create table if not exists order_lookup(order_index TEXT, user_id TEXT, order_ref TEXT, price REAL, yesterday_volume INT, today_volume INT)'
-            conn.execute(command)
-            command = 'insert into order_lookup(order_index , user_id , order_ref , yesterday_volume , today_volume ) select order_index , user_id , order_ref  , yesterday_volume , today_volume from order_lookup_temp'
-            conn.execute(command)
-            command = 'drop table order_lookup_temp;'
-            conn.execute(command)
-            conn.commit()
-            conn.close()
-            return 0
-        except:
-            error_msg = traceback.format_exc()
-            print('%s %s' % (trader_db_path, error_msg))
-            return -1
+        return 0
 
 
 databasecorrect = DatabaseCorrect()
